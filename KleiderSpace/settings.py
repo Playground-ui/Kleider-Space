5364c244b1e8a19b9f32aad72aac695743a1fa01
Playground-ui <maitysoumilya@gmail.com>
2022-06-13 11:14:48 +0530

    allowed host

--- KleiderSpace/settings.py
+++ KleiderSpace/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['Kleider-Space-env.eba-aji8yubb.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['Kleider-Space-env.eba-5kd7bvyc.us-west-2.elasticbeanstalk.com']
 
 
 # Application definition
